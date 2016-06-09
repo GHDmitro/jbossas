cd3ffacc52f07d3d37b242a11882c62ac423a17e
GHDmitro <d.timosh1996@gmail.comd.timosh1996@gmail.comd.timosh1996@gmail.com>
2016-06-09 14:57:52 +0300

    Deploy

--- src/main/java/pac/configuration/AppConfig.java
+++ src/main/java/pac/configuration/AppConfig.java
@@ -67,8 +67,8 @@ public class AppConfig extends WebMvcConfigurerAdapter{
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("com.mysql.jdbc.Driver");
         ds.setUrl("jdbc:mysql:$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/");
-        ds.setUsername("adminHwSBQHA");
-        ds.setPassword("yfxFjPEGEqtu");
+        ds.setUsername("adminB6yetsT");
+        ds.setPassword("dvGHf2pAnxYj");
 //        Map<String, String>
         Properties properties = new Properties();
         properties.setProperty("useUnicode", "true");
